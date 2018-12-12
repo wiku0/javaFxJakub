448f00fc75c7c130204a484e5cb069d55903c1b1
Wiktor Kamiński <wiktkaminski@gmail.com>
2018-12-12 11:18:31 +0100

    Select file added

--- src/main/java/com/student/jk/controller/Controller.java
+++ src/main/java/com/student/jk/controller/Controller.java
@@ -2,29 +2,21 @@ package com.student.jk.controller;
 
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+
+import java.io.File;
 
 
 public class Controller {
 
     @FXML
-    Label label1;
-
-    @FXML
-    TextField poleTekstowe;
-
- //   @FXML
-  //  TextField poleTekstowe;
-
-    @FXML
-    public void buttonClick(){
-        if(poleTekstowe.getText().trim().length()==0){
-            label1.setText("Nothing");
-        }else{
-            label1.setText(poleTekstowe.getText());
-        }
-
+    public void loadFromFile() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().size();
+        fileChooser.setTitle("Select database directory");
+        fileChooser.getExtensionFilters().add(new ExtensionFilter("Dokument tekstowy", "*.txt"));
+        File file = fileChooser.showOpenDialog(null);
     }
 
 }
